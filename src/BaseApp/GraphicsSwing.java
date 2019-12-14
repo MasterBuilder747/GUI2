fb50eafe3bf654991ccb4820132c9def1bb57245
Joseph <jaathunder@yahoo.com>
2019-12-13 18:35:36 -0800

    update swing application to work better

--- src/BaseApp/GraphicsSwing.java
+++ src/BaseApp/GraphicsSwing.java
@@ -13,7 +13,6 @@ import java.awt.event.KeyListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.util.ArrayList;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -23,28 +22,28 @@ import javax.swing.Timer;
 
 public class GraphicsSwing extends JFrame {
 
-    private int WIDTH = 512;
-    private int HEIGHT = 256;
+	private int WIDTH = 512;
+	private int HEIGHT = 256;
 
-	private Timer animationTimer = null;	
-	
+	private Timer animationTimer = null;
+
 	private GraphicPanelInner graphicsPanel;
 	private ControlPanelInner controlPanel;
-	
+
 	public GraphicsSwing ()
 	{
 
-		setTitle("JavaFX Graphics Application");
-				
+		setTitle("JavaSwing Graphics Application");
+
 		// -- size of the frame: width, height
 		setSize(WIDTH, HEIGHT);
-		
+
 		// -- center the frame on the screen
 		setLocationRelativeTo(null);
-		
+
 		// -- shut down the entire application when the frame is closed
 		//    if you don't include this the application will continue to
-		
+
 		//    run in the background and you'll have to kill it by pressing
 		//    the red square in eclipse
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -55,11 +54,11 @@ public class GraphicsSwing extends JFrame {
 
 		graphicsPanel = new GraphicPanelInner();
 		this.add(graphicsPanel, BorderLayout.CENTER);
-		
+
 		controlPanel = new ControlPanelInner();
 		this.add(controlPanel, BorderLayout.WEST);
 
-		
+
 		// -- Timer will generate an event every 10mSec once it is started
 		//    First parameter is the delay in mSec, second is the ActionListener
 		animationTimer = new Timer(10,
@@ -71,18 +70,18 @@ public class GraphicsSwing extends JFrame {
 				}
 		);
 
-        // -- paint the graphics canvas before the initial display
-        graphicsPanel.repaint();
-        
+		// -- paint the graphics canvas before the initial display
+		graphicsPanel.repaint();
+
 		// -- show the frame on the screen
 		setVisible(true);
-	
-        // -- set keyboard focus to the graphics panel
+
+		// -- set keyboard focus to the graphics panel
 		graphicsPanel.setFocusable(true);
-        graphicsPanel.requestFocus();
-		
+		graphicsPanel.requestFocus();
+
 	}
-	
+
 
 	// -- Inner class for the graphics panel
 	public class GraphicPanelInner extends JPanel implements MouseMotionListener {
@@ -98,50 +97,54 @@ public class GraphicsSwing extends JFrame {
 			super();
 			this.setBackground(Color.white);
 			prepareActionHandlers();
-			
+
 			this.addMouseMotionListener(this);
 		}
-		
-		
-        private void prepareActionHandlers()
-        {
+
+
+		private void prepareActionHandlers()
+		{
 			// -- The JPanel can have a mouse listener if desired
 			this.addMouseListener(new MouseListener() {
 
-				public void mouseClicked(MouseEvent event) {
-				}
+									  public void mouseClicked(MouseEvent event) {
+										  System.out.println("Mouse clicked at " + event.getX() + ", " + event.getY());
+									  }
 
-				public void mouseEntered(MouseEvent event) {
-				}
+									  public void mouseEntered(MouseEvent event) {
+									  }
 
-				public void mouseExited(MouseEvent event) {
-				}
+									  public void mouseExited(MouseEvent event) {
+									  }
 
-				public void mousePressed(MouseEvent event) {
-	            	if (event.getButton() == MouseEvent.BUTTON1) {
-	            	}
-	            	else if (event.getButton() == MouseEvent.BUTTON3) {
-	            	}
-	            	graphicsPanel.requestFocus();
-				}
+									  public void mousePressed(MouseEvent event) {
+										  if (event.getButton() == MouseEvent.BUTTON1) {
+											  System.out.println("Mouse BUTTON1");
+										  }
+										  else if (event.getButton() == MouseEvent.BUTTON3) {
+											  System.out.println("Mouse BUTTON2");
+										  }
+										  graphicsPanel.requestFocus();
+									  }
 
-				public void mouseReleased(MouseEvent event) {
-	            	if (event.getButton() == MouseEvent.BUTTON1) {
-	            	}
-	            	else if (event.getButton() == MouseEvent.BUTTON3) {
-	            	}
-	            	graphicsPanel.requestFocus();
-	            	repaint();
-				}
-				
-			}
+									  public void mouseReleased(MouseEvent event) {
+										  if (event.getButton() == MouseEvent.BUTTON1) {
+										  }
+										  else if (event.getButton() == MouseEvent.BUTTON3) {
+										  }
+										  graphicsPanel.requestFocus();
+										  repaint();
+									  }
+
+								  }
 			);
-			
+
 			// -- keyboard listener
 			this.addKeyListener(new KeyListener() {
 
 				@Override
 				public void keyTyped(KeyEvent event) {
+					System.out.println("key pressed " + event.getKeyChar());
 				}
 
 				@Override
@@ -153,30 +156,31 @@ public class GraphicsSwing extends JFrame {
 				public void keyReleased(KeyEvent event) {
 					graphicsPanel.repaint();
 				}
-				
+
 			});
-        }
-        
+		}
+
 		@Override
 		public void mouseDragged(MouseEvent event) {
+			System.out.println("Mouse dragged " + event.getX() + ", " + event.getY());
 			repaint();
 		}
 
 		@Override
 		public void mouseMoved(MouseEvent e) {
 			// TODO Auto-generated method stub
-			
+
 		}
-      
+
 		// -- this override sets the desired size of the JPanel which is
 		//    used by some layout managers -- default desired size is 0,0
 		//    which is, in general, not good -- will pull from layout manager
-		public Dimension getPreferredSize() 
+		public Dimension getPreferredSize()
 		{
 			return new Dimension(50, 50);
 		}
-		
-		// -- this override is where all the painting should be done. 
+
+		// -- this override is where all the painting should be done.
 		//    DO NOT call it directly. Rather, call repaint() and let the
 		//    event handling system decide when to call it
 		//    DO NOT put graphics function call elsewhere in the code, although
@@ -184,91 +188,98 @@ public class GraphicsSwing extends JFrame {
 		//    display
 		public void paint(Graphics g)
 		{
-			// -- the base class paintComponent(g) method ensures 
+			// -- the base class paintComponent(g) method ensures
 			//    the drawing area will be cleared properly. Do not
 			//    modify any attributes of g prior to sending it to
 			//    the base class
 			super.paintComponent(g);
-			
+
 			// -- for legacy reasons the parameter comes in as type Graphics
 			//    but it is really a Graphics2D object. Cast it up since the
 			//    Graphics2D class is more capable
 			Graphics2D g2d = (Graphics2D)g;
-        	int height = this.getHeight();
-        	int width = this.getWidth();
-        	
+			int height = this.getHeight();
+			int width = this.getWidth();
 
-       	
-        	
- 		}
+			g2d.setColor(Color.RED);
+			g2d.drawLine(0,  0,  width,  height);
+
+			// -- you can display a BufferedImage here
+			//g2d.drawImage(img, dx1, dy1, dx2, dy2, sx1, sy1, sx2, sy2, observer)
+
+
+		}
 
 	}
-	
+
 	// -- Inner class for control panel
 	public class ControlPanelInner extends JPanel {
 
-        private int nButtons = 6;
+		private int nButtons = 6;
 		private JButton buttons[];
-		
+
 		JTextField textField;
-		
+
 		public ControlPanelInner ()
 		{
-	        // -- set up buttons
+			// -- set up buttons
 			prepareButtonHandlers();
 
 			// -- add buttons to panel layout manager
 			setLayout(new GridLayout(10, 1, 2, 2));
-            for (int i = 0; i < buttons.length; ++i) {
-            	this.add(buttons[i]);
-            	if (i == 1) {
-            		textField = new JTextField();
-            		//textField.setWidth(60);
-            		this.add(textField);
-            	}
-            }
+			for (int i = 0; i < buttons.length; ++i) {
+				this.add(buttons[i]);
+				if (i == 1) {
+					textField = new JTextField();
+					//textField.setWidth(60);
+					this.add(textField);
+				}
+			}
 
-			
+
 		}
 
 		private void prepareButtonHandlers()
-        {
-        	buttons = new JButton[nButtons];
-        	for (int i = 0; i < buttons.length; ++i) {
-                buttons[i] = new JButton();
-                buttons[i].setText("Button " + i); 
-                buttons[i].addActionListener(
-    					new ActionListener() {
-    						public void actionPerformed(ActionEvent actionEvent) {
-    	                    	if (actionEvent.getSource() == buttons[0]) {
-    	                    		animationTimer.start();
-    	                    	}
-    	                    	else if (actionEvent.getSource() == buttons[nButtons - 1]) {
-    	                    		animationTimer.stop();
-    	                    	}
-    	                    	else if (actionEvent.getSource() == buttons[1]) {
-    	                    		System.out.println(textField.getText());
-    	                    	}
-    	                    	// -- process the button
-    	                        System.out.println(actionEvent.getSource().toString());
+		{
+			buttons = new JButton[nButtons];
+			for (int i = 0; i < buttons.length; ++i) {
+				buttons[i] = new JButton();
+				buttons[i].setText("Button " + i);
+				buttons[i].addActionListener(
+						new ActionListener() {
+							public void actionPerformed(ActionEvent actionEvent) {
+								if (actionEvent.getSource() == buttons[0]) {
+									animationTimer.start();
+								}
+								else if (actionEvent.getSource() == buttons[nButtons - 1]) {
+									animationTimer.stop();
+								}
+								else if (actionEvent.getSource() == buttons[2]) {
+									System.out.println("2nd button pressed");
+								}
+								else if (actionEvent.getSource() == buttons[1]) {
+									System.out.println(textField.getText());
+								}
+								// -- process the button
+								System.out.println(actionEvent.getSource().toString());
 
-    	                        // -- and return focus back to the graphics panel
-    	                        graphicsPanel.requestFocus();
-   												
-    						}
-    					}
-    				);
-                
-          	}
-        }
-		
-		public Dimension getPreferredSize() 
+								// -- and return focus back to the graphics panel
+								graphicsPanel.requestFocus();
+
+							}
+						}
+				);
+
+			}
+		}
+
+		public Dimension getPreferredSize()
 		{
 			return new Dimension(100, 500);
 		}
 
 	}
-	
+
 	public static void main (String[] args)
 	{
 		new GraphicsSwing();
